a45c1124bbbe7e1a22a6f905ce6a68c04d458d0f
gns <infiwang@proton.me>
2024-08-16 16:31:27 +0800

    riscv(misc): update copyright date

--- src/lj_emit_riscv.h
+++ src/lj_emit_riscv.h
@@ -1,6 +1,6 @@
 /*
 ** RISC-V instruction emitter.
-** Copyright (C) 2005-2022 Mike Pall. See Copyright Notice in luajit.h
+** Copyright (C) 2005-2023 Mike Pall. See Copyright Notice in luajit.h
 */
 
 static intptr_t get_k64val(ASMState *as, IRRef ref)
